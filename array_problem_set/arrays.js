f41d54ce2fa5d4d8dea2d1027f7f85eb162faee7
kevfallon97 <kfallon@tcd.ie>
2020-09-25 13:01:17 +0100

    adding isUniform exercise

--- array_problem_set/arrays.js
+++ array_problem_set/arrays.js
@@ -3,12 +3,28 @@
 // and prints out the element in the array in reverse order
 // dont reverse the array itself
 
-var nums = [1,2,3,4]
+var nums = [1,2,3,4];
 
 function printReverse(nums){
 	for (var i = 1; i <= nums.length; i++){
 		console.log(nums[nums.length-i])
 	}
 }
-
 printReverse(nums);
+
+// write a function isUniform() which takes an array as an arguement and returns true 
+// if all elements in the array are identical
+
+var nonUniform = [1,2,3,4];
+function isUniform(arr){
+	for (var i = 0; i < arr.length; i++){
+		if (arr[0] !== arr[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+isUniform(nonUniform);
+
+
