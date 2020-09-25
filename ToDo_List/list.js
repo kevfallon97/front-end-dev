46f9842d54127847f43fa0d50851ca83182a3b09
kevfallon97 <kfallon@tcd.ie>
2020-09-25 12:34:56 +0100

    updating code and cleaning up logic

--- ToDo_List/list.js
+++ ToDo_List/list.js
@@ -6,13 +6,13 @@ window.setTimeout(function() {
 	var input = prompt("What would you like to do?");
 
 	while(input !== "quit"){
+		// handle input
 		if (input === "list"){
-			console.log(todos);
+			listTodos();
 		} else if (input === "new"){
-			//ask for new todo
-			var newTodo = prompt("Enter new todo");
-			//add to todos array
-			todos.push(newTodo);
+			addTodo();
+		} else if (input === "delete"){
+			deleteTodo();
 		}
 		var input = prompt("What would you like to do?");
 	}
@@ -21,3 +21,22 @@ window.setTimeout(function() {
 
 }, 500);
 
+function listTodos(){
+	console.log("*******");
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("*******");
+}
+
+function addTodo(){
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	//add to todos array
+	todos.push(newTodo);
+}
+
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index, 1);
+}
